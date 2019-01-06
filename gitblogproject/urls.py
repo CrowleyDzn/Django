bcff2a5f9c0b5a64e8936a1a803a6abc3d6fdc39
CrowleyDzn <41930252+CrowleyDzn@users.noreply.github.com>
2019-01-06 21:01:54 -0200

    Delete urls.py

--- gitblogproject/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url, include
-from core.views import home, lista
-from django.urls import path
-
-urlpatterns = [
-	url(r'^$', lista),
-	path('padrao/', home),
-	path('accounts/', include('django.contrib.auth.urls')),
-]
